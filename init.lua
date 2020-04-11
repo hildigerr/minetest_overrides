b56634d70837f04ea79ca6130ea70674d8782bf1
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2020-04-11 04:15:38 -0700

    s/my_preference/minetest_overrides/

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -----------------My Recipe and Node Redefinitions and Additions-----------------
 
 ---Stone and Cobblestone Switch---
-dofile(minetest.get_modpath("my_preference").."/stoned.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/stoned.lua")
 
 -- Stick from dry shrub
 minetest.register_craft({
@@ -20,37 +20,37 @@ minetest.register_craft({
 })
 
 ---gates_wooden: [Comment out original Crafts section]
-dofile(minetest.get_modpath("my_preference").."/gates_wooden_changes.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/gates_wooden_changes.lua")
 
 ---homedecor:
-dofile(minetest.get_modpath("my_preference").."/homedecor_changes.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/homedecor_changes.lua")
 
 ---moreblocks:
-dofile(minetest.get_modpath("my_preference").."/moreblocks_changes.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/moreblocks_changes.lua")
 
 ---moreores:
 --copper tools and rebalance tool characteristics [also includes desert_uses stone tools]
-dofile(minetest.get_modpath("my_preference").."/tool_balance.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/tool_balance.lua")
 
 ---plantlife:
-dofile(minetest.get_modpath("my_preference").."/plantlife_changes.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/plantlife_changes.lua")
 
 ---Dye Recipes (instead of Unified Dyes)
-dofile(minetest.get_modpath("my_preference").."/my_dyes.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/my_dyes.lua")
 
 --Teleporters:[comment out lines 172-186]
-dofile(minetest.get_modpath("my_preference").."/teleporters_changes.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/teleporters_changes.lua")
 
 ----Snow:
-dofile(minetest.get_modpath("my_preference").."/snow_changes.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/snow_changes.lua")
 
 
 ---wool:
-dofile(minetest.get_modpath("my_preference").."/wool_recipes.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/wool_recipes.lua")
 
 
 ----[Modify Stairs to produce 8 instead of 4]----
 
 
 ---TEXTURE PACK ANIMATIONS
-dofile(minetest.get_modpath("my_preference").."/texture_pack_additions.lua")
+dofile(minetest.get_modpath("minetest_overrides").."/texture_pack_additions.lua")
